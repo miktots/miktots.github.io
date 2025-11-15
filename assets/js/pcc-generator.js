4725590201f128e8a8544177305b8de9f5c2a78b
dumaikota <febykaseps@gmail.com>
2025-11-15 10:30:58 +0700

    w

--- assets/js/pcc-generator.js
+++ assets/js/pcc-generator.js
@@ -1,29 +1,27 @@
 document.getElementById('pcc-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Get RouterOS version
     var routerosVersion = document.getElementById('routeros-version').value;
 
-    // Get Source IP, Destination IP, Interface, Gateway, and Routing Mark
     var srcIp = document.getElementById('src-ip').value;
     var dstIp = document.getElementById('dst-ip').value;
     var interface = document.getElementById('interface').value;
     var gateway = document.getElementById('gateway').value;
     var routingMark = document.getElementById('mark-routing').value;
 
-    // Get WAN sources
     var wanSources = [];
     var wanInputs = document.querySelectorAll('input[name="wan-source"]');
     wanInputs.forEach(function(input) {
         wanSources.push(input.value);
     });
 
-    // Generate configuration based on RouterOS version and inputs
     var config = '';
     if (routerosVersion === '6') {
-        config += "# RouterOS 6 Configuration\n";
+        config += "# RouterOS 6 Configuration
+";
     } else if (routerosVersion === '7') {
-        config += "# RouterOS 7 Configuration\n";
+        config += "# RouterOS 7 Configuration
+";
     }
 
     config += `
@@ -32,7 +30,6 @@ add chain=prerouting src-address=${srcIp} dst-address=${dstIp} action=mark-conne
 add chain=prerouting connection-mark=conn1 action=mark-routing new-routing-mark=${routingMark}
     `;
 
-    // Add multiple WAN sources
     wanSources.forEach(function(wan, index) {
         config += `
 # WAN Source ${index + 1}
@@ -41,17 +38,14 @@ add chain=prerouting connection-mark=conn${index + 1} action=mark-routing new-ro
         `;
     });
 
-    // Add routing rule
     config += `
 /ip route
 add gateway=${gateway} routing-mark=${routingMark}
     `;
 
-    // Output the generated configuration to textarea
     document.getElementById('output').value = config;
 });
 
-// Add WAN source input dynamically
 document.getElementById('add-wan-source').addEventListener('click', function() {
     var wanSourceDiv = document.createElement('div');
     wanSourceDiv.classList.add('wan-source');
